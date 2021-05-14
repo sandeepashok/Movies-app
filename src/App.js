65801c585d9996d62dff9dacc34151528cc6e9e4
Sandeep Ashok <51964832+sandeepashok@users.noreply.github.com>
2021-05-14 13:36:15 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -96,24 +96,6 @@ export default function App() {
       "video": false,
       "vote_average": 7.7,
       "vote_count": 93
-    },
-    {
-      "adult": false,
-      "backdrop_path": "/zDq2pwPyt4xwSFHKUoNN2LohDWj.jpg",
-      "genre_ids": [
-        27
-      ],
-      "id": 632357,
-      "original_language": "en",
-      "original_title": "The Unholy",
-      "overview": "Alice, a young hearing-impaired girl who, after a supposed visitation from the Virgin Mary, is inexplicably able to hear, speak and heal the sick. As word spreads and people from near and far flock to witness her miracles, a disgraced journalist hoping to revive his career visits the small New England town to investigate. When terrifying events begin to happen all around, he starts to question if these phenomena are the works of the Virgin Mary or something much more sinister.",
-      "popularity": 844.368,
-      "poster_path": "/b4gYVcl8pParX8AjkN90iQrWrWO.jpg",
-      "release_date": "2021-03-31",
-      "title": "The Unholy",
-      "video": false,
-      "vote_average": 5.5,
-      "vote_count": 81
     }
   ])
   const [playlists, setPlaylists] = useState([
